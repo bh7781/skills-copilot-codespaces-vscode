8324b76c43f63c6611fb95136696e5064781423f
Chinmay Shelke <chinmay.s077@gmail.com>
2024-12-24 12:50:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberSkills = document.getElementById("memberSkills");
+  var memberSkillsList = document.getElementById("memberSkillsList");
+  var memberSkillsClose = document.getElementById("memberSkillsClose");
+  
+  memberSkills.style.display = "block";
+  memberSkillsList.style.display = "block";
+  memberSkillsClose.style.display = "block";
+  member.style.display = "none";
+}
